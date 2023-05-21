f3b3c5e3f01d476e7e33a243a0a2217d7119c53c
Stephane Moreau <smoreau@logikdev.com>
2023-05-21 18:06:41 +0200

    Change version of the plugin

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_document_move do
   name 'Move Document plugin'
   author 'Stephane Moreau'
   description 'A plugin which allows to move documents from a project to another.'
-  version '0.1.0'
+  version '0.2.0'
   url 'https://github.com/smoreau/redmine_document_move'
   author_url 'http://www.logikdev.com'
 
