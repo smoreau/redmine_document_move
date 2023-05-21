f1400f99847194329056fe1edb76d3684641525b
Stephane Moreau <smoreau@logikdev.com>
2023-05-21 18:04:02 +0200

    Add a dependency to the redmine_base_deface plugin

--- app/overrides/documents/show.rb
+++ app/overrides/documents/show.rb
@@ -1,4 +1,5 @@
 Deface::Override.new :virtual_path => 'documents/show',
+  :original => '3327e67137d2ee4fe686eb45f59819ad095f65bd',
   :name => 'add-move-link-to-documents-show',
   :insert_top => 'div.contextual',
   :partial => 'documents/show_move'
--- init.rb
+++ init.rb
@@ -3,13 +3,6 @@ $LOAD_PATH.unshift "#{File.dirname(__FILE__)}/lib"
 require 'documents_helper_patch'
 require 'documents_controller_patch'
 
-# Little hack for deface in redmine:
-# - redmine plugins are not railties nor engines, so deface overrides are not detected automatically
-# - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
-# - hack consists in adding "app/overrides" path of the plugin in Redmine's main #paths
-Rails.application.paths["app/overrides"] ||= []
-Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides", __FILE__)
-
 Redmine::Plugin.register :redmine_document_move do
   name 'Move Document plugin'
   author 'Stephane Moreau'
